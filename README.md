0ebda1a8e44a2ee96fd8b633e4f35a6aaa6e10a7
ranggaangga <36304550+ranggaangga@users.noreply.github.com>
2025-10-22 09:49:17 +0700

    delete all file

--- README.md
+++
@@ -1,1 +0,0 @@
-# AR_Penempatan_Produk
--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  server: {
-    allowedHosts: ['.ngrok-free.dev'],
-    host: true,
-    port: 5173
-  }
-})
